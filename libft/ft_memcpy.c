8be82ff484c980472e5029cba996495d454db28b
qcocusse <qcocusse@e2r11p1.42.fr>
2014-11-08 15:50:05 +0100

    ft_memcpy.c

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qcocusse <qcocusse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/08 15:00:46 by qcocusse          #+#    #+#             */
+/*   Updated: 2014/11/08 15:14:44 by qcocusse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void		ft_memcpy(void *dst, const void *str, size_t n)
+{
+	size_t				i;
+	unsigned char		*s1;
+	unsigned char		*s2;
+
+	s1 = (unsigned char *)dst;
+	s2 = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	return (dst);
+}
