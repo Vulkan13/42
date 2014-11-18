2e537ed4f72b7cc0c66967e99ac947f089b3ab18
qcocusse <qcocusse@e3r10p1.42.fr>
2014-11-18 08:36:34 +0100

    correction + ajout

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,25 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: qcocusse <qcocusse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/04 11:37:43 by qcocusse          #+#    #+#             */
-/*   Updated: 2014/11/18 08:30:14 by qcocusse         ###   ########.fr       */
+/*   Created: 2014/11/18 08:29:02 by qcocusse          #+#    #+#             */
+/*   Updated: 2014/11/18 08:31:50 by qcocusse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char		*ft_strcpy(char *s1, char *s2)
-{
-	int		i;
+#include "libft.h"
 
-	i = 0;
-	while (s1[i] && s2[i])
-	{
-		s1[i] = s2[i];
-		i++;
-	}
-	s1[i] = '\0';
-	return (s1);
+void		*ft_memmove(void *dst, const void *src, size_t n)
+{
+	unsigned char	*str;
+
+	str = (unsigned char *)malloc (sizeof(*str) * n);
+	ft_memcpy (str, src, n);
+	ft_memcpy (dst, str, n);
+	free (str);
+	return (dst);
 }
