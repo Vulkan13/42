41c4ecf590870c15c7916e9ea22e158ff1e59776
qcocusse <qcocusse@e1r4p3.42.fr>
2015-02-09 12:53:20 +0100

    map et mapi

---
+++ libft/ft_strmap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qcocusse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/09 08:48:49 by qcocusse          #+#    #+#             */
+/*   Updated: 2015/02/09 08:53:47 by qcocusse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char		*ft_strmap(char const *s, char (*f)(char))
+{
+	int		i;
+	int		size;
+	char	*str;
+
+	i = 0;
+	if (!s || !f)
+		return (NULL);
+	size = ft_strlen(s);
+	str = malloc(size + 1);
+	str[size] = '\0';
+	while (i < size)
+	{
+		str[i] = (*f)(s[i]);
+		i++;
+	}
+	return (str);
+}
---
+++ libft/ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qcocusse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/09 08:57:34 by qcocusse          #+#    #+#             */
+/*   Updated: 2015/02/09 09:02:34 by qcocusse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char		*ft_strmapi(char const *s,char (*f)(unsigned int, char))
+{
+	int		size;
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!s || !f)
+		return (NULL);
+	size = ft_strlen(s);
+	str = malloc(size + 1);
+	str[size] = '\0';
+	while (i < size)
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	return (str);
+}
